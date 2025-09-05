4308355041bdd42fecebceff1d0e6ca1f9f51799
M4tiasG <mataxd4@gmail.com>
2025-09-05 23:37:14 +0000

    Arreglo de PID hijo // Actividad Wait Exit en SO/Procesos

--- Sistemas Operativos/Procesos/ActividadWaitExit.c
+++ Sistemas Operativos/Procesos/ActividadWaitExit.c
@@ -9,22 +9,22 @@ int main(){
 
     printf("PADRE: Esperando a que los hijos terminen de dormir\n");
     if ((retornoFork1 = fork()) == 0) { // proceso hijo 1
-        printf("Soy el hijo 1, mi PID es %d y mi PPID es %d\n durmiendo...\n\n", getegid(), getppid());
+        printf("Soy el hijo 1, mi PID es %d y mi PPID es %d\n durmiendo...\n\n", getpid(), getppid());
         sleep(2);
-        printf("Hijo 1 de despertado y mi PID es %d\n", getegid());
+        printf("Hijo 1 de despertado y mi PID es %d\n", getpid());
         exit(0);
 
     }else{
         if ((retornoFork2 = fork()) == 0) { // proceso hijo 2
-        printf("Soy el hijo 2, mi PID es %d y mi PPID es %d\n durmiendo...\n\n", getegid(), getppid());
+        printf("Soy el hijo 2, mi PID es %d y mi PPID es %d\n durmiendo...\n\n", getpid(), getppid());
         sleep(4);
-        printf("Hijo 2 de despertado y mi PID es %d\n", getegid());
+        printf("Hijo 2 de despertado y mi PID es %d\n", getpid());
         exit(0);
         }else{
             if ((retornoFork3 = fork()) == 0) { // proceso hijo 3
-                printf("Soy el hijo 3, mi PID es %d y mi PPID es %d\n durmiendo...\n\n", getegid(), getppid());
+                printf("Soy el hijo 3, mi PID es %d y mi PPID es %d\n durmiendo...\n\n", getpid(), getppid());
                 sleep(6);
-                printf("Hijo 3 de despertado y mi PID es %d\n", getegid());
+                printf("Hijo 3 de despertado y mi PID es %d\n", getpid());
                 exit(0);
             }else { // proceso padre
                 wait(NULL);
